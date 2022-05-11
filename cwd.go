6289c5d725b3f02e130b1268a260906556f23bf3
thisisaaronland <aaron@localhost>
2022-05-11 15:12:54 -0700

    improved docs and error reporting

--- cwd.go
+++ cwd.go
@@ -7,6 +7,7 @@ import (
 	"os"
 )
 
+// CwdWriter is a struct that implements the Writer interface for writing documents to the current working directory.
 type CwdWriter struct {
 	Writer
 	writer Writer
@@ -30,6 +31,9 @@ func init() {
 	}
 }
 
+// NewCwdWriter returns a new `CwdWriter` instance for writing documents to the current working directory.
+// Although 'uri' is a required parameter (in order to comply with the `WriterInitializationFunc`
+// interface) it is ignored and may be empty.
 func NewCwdWriter(ctx context.Context, uri string) (Writer, error) {
 
 	cwd, err := os.Getwd()
@@ -52,15 +56,18 @@ func NewCwdWriter(ctx context.Context, uri string) (Writer, error) {
 	return wr, nil
 }
 
+// Write copies the content of 'fh' to 'path'.
 func (wr *CwdWriter) Write(ctx context.Context, path string, fh io.ReadSeeker) (int64, error) {
 
 	return wr.writer.Write(ctx, path, fh)
 }
 
+// WriterURI returns the final URI for 'path'
 func (wr *CwdWriter) WriterURI(ctx context.Context, path string) string {
 	return wr.writer.WriterURI(ctx, path)
 }
 
+// Close closes the underlying writer mechanism.
 func (wr *CwdWriter) Close(ctx context.Context) error {
 	return wr.writer.Close(ctx)
 }
