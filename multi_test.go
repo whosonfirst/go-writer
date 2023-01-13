0e983233f224e5d21653b7f9d04ef82fdf2b3fc8
sfomuseumbot <sfomuseumbot@localhost>
2023-01-12 22:05:07 -0800

    improved tests

--- multi_test.go
+++ multi_test.go
@@ -2,7 +2,11 @@ package writer
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"log"
 	"strings"
+	"sync"
 	"testing"
 )
 
@@ -85,6 +89,77 @@ func TestAsyncMultiWriter(t *testing.T) {
 		t.Fatalf("Failed to write, %v", err)
 	}
 
+	err = mw.Close(ctx)
+
+	if err != nil {
+		t.Fatalf("Failed to close writer, %v", err)
+	}
+}
+
+func TestMultiWriterWithOptions(t *testing.T) {
+
+	ctx := context.Background()
+
+	schemes := []string{
+		"stdout://",
+		"null://",
+	}
+
+	writers := make([]Writer, len(schemes))
+
+	for idx, s := range schemes {
+
+		wr, err := NewWriter(ctx, s)
+
+		if err != nil {
+			t.Fatalf("Failed to create '%s' writer, %v", s, err)
+		}
+
+		writers[idx] = wr
+	}
+
+	opts := &MultiWriterOptions{
+		Writers: writers,
+		Async:   true,
+		Verbose: true,
+	}
+
+	mw, err := NewMultiWriterWithOptions(ctx, opts)
+
+	if err != nil {
+		t.Fatalf("Failed to create new multi writer with options, %v", err)
+	}
+
+	logger := log.Default()
+
+	err = mw.SetLogger(ctx, logger)
+
+	if err != nil {
+		t.Fatalf("Failed to set logger, %v", err)
+	}
+
+	wg := new(sync.WaitGroup)
+
+	for i := 0; i < 10; i++ {
+
+		r := strings.NewReader("Hello world\n")
+		wg.Add(1)
+
+		go func(i int, r io.ReadSeeker) {
+
+			defer wg.Done()
+			path := fmt.Sprintf("debug-%d", i)
+
+			_, err = mw.Write(ctx, path, r)
+
+			if err != nil {
+				t.Fatalf("Failed to write, %v", err)
+			}
+		}(i, r)
+
+	}
+
+	wg.Wait()
 	err = mw.Close(ctx)
 
 	if err != nil {
